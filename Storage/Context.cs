83de8828db93f19ec37470fe2b881095c498471a
Максим Малахов <Beasteamever@gmail.com>
2018-07-02 15:58:27 +0300

    add models lib

---
+++ Storage/Context.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace Storage
+{
+    public class Context : DbContext
+    {        
+        public Context()
+        {           
+        }
+       
+    }
+}
